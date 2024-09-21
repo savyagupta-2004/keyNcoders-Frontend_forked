fb7f44d26fedfbc4b060d83e49dc83c1590ba671
AamukthaThammisetty <aamukthasetty2005@gmail.com>
2024-09-21 15:49:02 +0530

    updated question page

---
+++ src/pages/CodeEditor.jsx
@@ -0,0 +1,138 @@
+import React, { useState,useRef } from 'react';
+import Navbar from '../utilities/Navbar';
+import Editor from '@monaco-editor/react';
+const CodeEditor = ({ theme, handleThemeSwitch }) => {
+  const [currentView, setCurrentView] = useState('description'); // State to manage the current view
+  const defaultCode = {
+    // javascript: "// JavaScript\nconsole.log('Hello, World!');",
+    // typescript: "// TypeScript\nconsole.log('Hello, World!');",
+    python: "# Python\nprint('Hello, World!')",
+    java: "// Java\npublic class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println(\"Hello, World!\");\n    }\n}",
+    c: "/* C */\n#include <stdio.h>\n\nint main() {\n    printf(\"Hello, World!\\n\");\n    return 0;\n}",
+    cpp: "// C++\n#include <iostream>\n\nint main() {\n    std::cout << \"Hello, World!\" << std::endl;\n    return 0;\n}",
+  };
+  const editorRef = useRef(null);
+  const [language, setLanguage] = useState('java'); // Initial language state
+  const [code, setCode] = useState(defaultCode.java); // Initial code state
+  const [ans,setans]=useState(false)
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    alert(editorRef.current.getValue());
+  }
+
+  function handleLanguageChange(event) {
+    const selectedLanguage = event.target.value;
+    setLanguage(selectedLanguage);
+    setCode(defaultCode[selectedLanguage]); // Update code based on selected language
+  }
+  const questions = [
+    {
+      num: "1.1",
+      name: "Longest Subsequence",
+      des: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati sapiente laborum error nobis, explicabo ducimus minima vitae ex tempora nisi dolor ipsa dignissimos iure facilis tempore! Neque veniam vel facere.",
+      exnum: "1.1.1",
+      submission: {
+        true: "Correct",
+        false: "Incorrect",
+      },
+      solution: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit iste error sequi ducimus dolores quidem numquam, magni ipsa amet quibusdam laborum laboriosam atque omnis saepe vero aspernatur, minima quas assumenda?",
+      exDes: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati sapiente laborum error nobis, explicabo ducimus minima vitae ex tempora nisi dolor ipsa dignissimos iure facilis tempore! Neque veniam vel facere."
+    }
+  ];
+
+  const handleViewChange = (view) => {
+    setCurrentView(view);
+  };
+
+  return (
+    <div>
+      <Navbar handleThemeSwitch={handleThemeSwitch} theme={theme} />
+      <section>
+        <div className='grid g grid-cols-2 gap-2 bg-black'>
+          <div className='col-span-1 p-5  bg-[#262626] h-screen'>
+            <div className="flex overflow-x-auto mt-16 whitespace-nowrap">
+              <button
+                className={`inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700 border-b border-gray-300 sm:text-base dark:border-gray-500  dark:text-white whitespace-nowrap focus:outline-none ${currentView === 'description' ? 'dark:border-gray-500 rounded-t-md border border-b-0' : 'bg-transparent'}`}
+                onClick={() => handleViewChange('description')}
+              >
+                Description
+              </button>
+              <button
+                className={`inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700  border-b border-gray-300 sm:text-base dark:border-gray-500 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 dark:hover:border-gray-300 ${currentView === 'solution' ? 'dark:border-gray-500 rounded-t-md border border-b-0' : 'bg-transparent'}`}
+                onClick={() => handleViewChange('solution')}
+              >
+                Solution
+              </button>
+              <button
+                className={`inline-flex items-center h-12 px-4 py-2 text-sm text-center text-gray-700  border-b border-gray-300 sm:text-base dark:border-gray-500 dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 dark:hover:border-gray-300 ${currentView === 'submission' ? 'dark:border-gray-500 rounded-t-md border border-b-0' : 'bg-transparent'}`}
+                onClick={() => handleViewChange('submission')}
+              >
+                Submission
+              </button>
+            </div>
+            <div className='text-white'>
+              {questions.map((question, index) => (
+                <div key={index} className="space-y-2"> 
+                  {currentView === 'description' && (
+                    <>
+                      <h1 className="mt-4 text-2xl">{question.num}: {question.name}</h1> 
+                      <p className="m-0 p-0">{question.des}</p> 
+                      <h1 className="m-0 p-0 text-xl">{question.exnum} Example</h1> 
+                      <p className="m-0 p-0">{question.exDes}</p> 
+                    </>
+                  )}
+                  {currentView === 'solution' && <p className="m-0 pt-4">{question.solution}</p>}
+                  {currentView === 'submission' && (
+                    <div>{
+                         ans&&
+                         <p className="m-0 pt-4">{question.submission.true}</p>
+                        }
+                       {
+                         !ans&&
+                         <p className="m-0 pt-4">{question.submission.false}</p>
+                        }
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='col-span-1 grid gap-2  grid-rows-3 '>
+            <div className='row-span-2  px-5 bg-[#262626]'>
+                <div className='mt-20 '>
+                    <select value={language} onChange={handleLanguageChange} className="text-sm bg-[#232222] ml-2 dark:border-gray-500 border  px-2 py-1 dark:text-white outline-none rounded-md h-full">
+                        <option>cpp</option>
+                        <option>c</option>
+                        <option>java</option>
+                        <option>python</option>
+                    </select>
+                </div>
+                <div>
+                <Editor
+                  className=' bg-[#232222] h-72 w-full m-2 dark:border-gray-500 border dark:text-white  outline-none rounded-md'
+                  language={language}
+                  value={code}
+                  onMount={handleEditorDidMount}
+                  options={{ selectOnLineNumbers: true }} // Optional editor option
+                 />
+                </div>  
+                <div className='flex gap-3 flex-row-reverse'>
+                <button className='border dark:border-gray-500 rounded-md dark:text-white px-2 py-1 '>Submit</button>
+                <button className='border dark:border-gray-500 rounded-md dark:text-white px-2 py-1 '>Run Code</button>
+                </div>
+            </div>
+            <div className='row-span-1  bg-[#262626]'>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default CodeEditor;
--- src/pages/PostLogin.jsx
+++ src/pages/PostLogin.jsx
@@ -1,76 +1,15 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Navbar from "../utilities/Navbar";
+import React, { useState } from "react";
+import PostloginCard from "../components/PostloginCard";
+import CustomCalendar from "../components/CustomCalendar";
+import Statistics from "../components/Statistics";
+import CircularProgress from "../components/CircularProgress";
+import PostloginNavbar from "../utilities/PostloginNavbar";
+import PostloginCourseCard from "../utilities/PostloginCourseCard";
 import LaptopSlider from "../components/LaptopSlider";
 import Footer from "../utilities/Footer";
-import debounce from "lodash.debounce";
-// in order to fetch user data
 
-import { Link } from "react-router-dom";
-
-const backendUrl = "https://keyncoders-main-backend.vercel.app";
-// const backendUrl = process.env.BACKEND_URL;
 const PostLogin = ({ theme, handleThemeSwitch }) => {
-  const [showStats, setShowstats] = useState(false);
-  const [userdetails, setuserdetails] = useState({});
-  const navigate = useNavigate();
-
-  const fetchShowStats = async () => {
-    const savedUser = JSON.parse(localStorage.getItem("savedUser"));
-    const userId = savedUser._id; // Access _id directly from savedUser object
-    console.log(userId);
-    const user_resetId = savedUser.resetPasswordToken;
-    console.log(user_resetId);
-
-    const courseId = "66b501e92bc0fdcb012c1449";
-    const token = localStorage.getItem("token");
-    try {
-      // Verify endpoint
-      const response = await fetch(`${backendUrl}/course/checkCourses`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`, // Include token in Authorization header
-        },
-        body: JSON.stringify({ userId, courseId }),
-      });
-
-      const json = await response.json();
-      // console.log(json);
-
-      if (response.status === 200) {
-        setShowstats(true);
-      } else {
-        setShowstats(false);
-      }
-    } catch (error) {
-      console.error("Error fetching showStats:", error);
-    }
-  };
-
-  const debouncedFetchShowStats = useCallback(
-    debounce(fetchShowStats, 300),
-    []
-  );
-  useEffect(() => {
-    const savedUser = localStorage.getItem("savedUser");
-    if (savedUser) {
-      setuserdetails(JSON.parse(savedUser));
-      debouncedFetchShowStats();
-    } else {
-      setShowstats(false);
-    }
-    return () => {
-      debouncedFetchShowStats.cancel();
-    };
-  }, [debouncedFetchShowStats]);
-
-  const handleNavigateDsa = () => {
-    navigate("/dsa");
-  };
-  const handleNavigatehome = () => {
-    navigate("/");
-  };
+  const [showStats, setShowstats] = useState(true);
 
   return (
     <div
@@ -78,117 +17,128 @@ const PostLogin = ({ theme, handleThemeSwitch }) => {
         theme === "dark" ? "bg-[#131313] text-white" : "bg-white text-black"
       }`}
     >
-      <Navbar handleThemeSwitch={handleThemeSwitch} theme={theme} />
+      <PostloginNavbar handleThemeSwitch={handleThemeSwitch} theme={theme} />
 
       {showStats ? (
-        <>
-          <div
-            className={`${
-              theme === "dark"
-                ? "bg-black"
-                : "bg-gradient-to-r from-[#ED374D] via-[#FA793F] to-[#FCB900] "
-            }`}
-          >
-            <div className=" mt-48 sm:mt-0 ">
-              <div
-                className={`w-full h-[90vh] md:flex justify-center items-center ${
-                  theme === "dark"
-                    ? "bg-black"
-                    : "bg-gradient-to-r from-[#ED374D] via-[#FA793F] to-[#FCB900]"
-                }`}
-              >
-                <div className="mx-10  flex flex-col lg:flex-row   items-center justify-between">
-                  <div>
-                    <h1 className="max-w-xl text-4xl leading-[1.1] md:text-5xl md:leading-[1.3] font-bold text-white dark:text-white">
-                      Hey{" "}
-                      <span className="text-black dark:text-yellow-200">
-                        {userdetails.name || "User"}
-                      </span>{" "}
-                      You’ve got a knack for smart decisions
-                    </h1>
-                    <p className="text-[#FFB453] font-semibold mt-4 dark:text-[#e87f7f] md:text-[28px] max-w-sm mb-5">
-                      Your batch starts on Sept 6
-                    </p>
-                    <Link target="_blank" to="https://rzp.io/l/y1Eux1i">
-                      <button
-                        onClick={handleNavigatehome}
-                        className="dark:bg-orange-600 bg-orange-500 shadow-lg hover:bg-orange-600 dark:hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-lg md:mt-0 mt-5 ml-0 md:mb-10 mb-6 sm:mb-8 md:ml-0 lg:ml-0 flex justify-center items-center"
-                      >
-                        Join Now
-                      </button>
-                    </Link>
-                  </div>
-                  <div className=" relative w-[110%] -ml-3 mx-auto  sm:w-[70%] sm:ml-20  md:w-[500%] md:justify-center max-w-md">
-                    <img
-                      src="../images/laptop.png"
-                      alt="Laptop Slider"
-                      className="w-full h-auto rounded-lg "
+        <div>
+          <div className="mt-8 flex ">
+            <PostloginCard theme={theme} />
+            <div>
+              <CustomCalendar theme={theme} />
+            </div>
+          </div>
+          <div className="text-2xl font-bold dark:white ml-[3%] mt-2">
+            My Statistics
+          </div>
+
+          <div className="flex justify-between">
+            <div className="flex flex-col w-[40rem] ml-8">
+              <div className="flex w-full">
+                <div className="">
+                  <Statistics />
+                </div>
+                <div className="flex flex-col justify-center items-center h-[300px] ml-10 w-[120px] space-y-2">
+                  <div
+                    className="flex justify-center items-center px-3  rounded-2xl w-full h-[90%]"
+                    style={{
+                      background:
+                        "linear-gradient(115.93deg, rgba(217, 217, 217, 0.152) 19.7%, rgba(115, 115, 115, 0.019) 139.96%)",
+                    }}
+                  >
+                    <CircularProgress
+                      label="Productivity"
+                      value="100"
+                      width="100"
                     />
-                    <div className=" absolute md:top-[8.3%] left-[11.5%] w-[76%] h-[65%] top-[7%] sm:top-[5%] lg:top-[11.6%]">
-                      <LaptopSlider />
-                    </div>
+                  </div>
+                  <div
+                    className="flex justify-center items-center px-2 rounded-2xl w-full h-[90%]"
+                    style={{
+                      background:
+                        "linear-gradient(115.93deg, rgba(217, 217, 217, 0.152) 19.7%, rgba(115, 115, 115, 0.019) 139.96%)",
+                    }}
+                  >
+                    <CircularProgress label="Quiz" value="75" width="100" />
                   </div>
                 </div>
               </div>
-              <Footer theme={theme} />
+            </div>
+            <div className="flex flex-col justify-center items-center w-[43.5rem] mr-8 h-fit ">
+              <PostloginCourseCard
+                theme={theme}
+                course="DSA"
+                topic="Linked List"
+                img="dsa-postlogin"
+                play={true}
+              />
+              <PostloginCourseCard
+                theme={theme}
+                course="UI/UX"
+                topic="Auto Layout"
+                img="hack"
+                play={false}
+              />
             </div>
           </div>
-        </>
+        </div>
       ) : (
-        <>
+        <div className="mt-[3.9rem]">
           <div
-            className={`${
-              theme === "dark"
-                ? "bg-black"
-                : "bg-gradient-to-r from-[#ED374D] via-[#FA793F] to-[#FCB900] "
+            className={`w-full h-[90vh] ${
+              theme == "dark"
+                ? "bg-[#1D1534]"
+                : "bg-gradient-to-r from-[#ED374D] via-[#FA793F] to-[#FCB900]"
             }`}
           >
-            <div className=" mt-48 sm:mt-0 ">
-              <div
-                className={`w-full h-[90vh] md:flex justify-center items-center ${
-                  theme === "dark"
-                    ? "bg-black"
-                    : "bg-gradient-to-r from-[#ED374D] via-[#FA793F] to-[#FCB900]"
-                }`}
-              >
-                <div className="mx-10  flex flex-col lg:flex-row   items-center justify-between">
-                  <div>
-                    <h1 className="mt-10 max-w-xl text-[38.8px]  leading-[1.1] md:text-5xl md:leading-[1.3] font-bold text-white dark:text-white">
-                      Hey{" "}
-                      <span className="text-black dark:text-yellow-200">
-                        {" "}
-                        {userdetails.name || "User"}
-                      </span>{" "}
-                      seems you haven't enrolled yet
-                    </h1>
-                    <p className="text-[#FFB453] font-semibold mt-2 dark:text-[#e87f7f] md:text-[28px] max-w-sm mb-5">
-                      Don't hesitate - Join now and get the finest available
-                    </p>
-                    <Link target="_blank" to="https://rzp.io/l/y1Eux1i">
-                      <button
-                        onClick={handleNavigatehome}
-                        className="dark:bg-orange-600 bg-orange-500 shadow-lg hover:bg-orange-600 dark:hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-lg md:mt-0 mt-5 ml-0 md:mb-10 mb-6 sm:mb-8 md:ml-0 lg:ml-0 flex justify-center items-center"
-                      >
-                        Join Now
-                      </button>
-                    </Link>
-                  </div>
-                  <div className=" relative w-[110%] -ml-3 mx-auto  sm:w-[70%] sm:ml-20  md:w-[500%] md:justify-center max-w-md">
-                    <img
-                      src="../images/laptop.png"
-                      alt="Laptop Slider"
-                      className="w-full h-auto rounded-lg "
-                    />
-                    <div className=" absolute md:top-[8.3%] left-[11.5%] w-[76%] h-[65%] top-[7%] sm:top-[5%] lg:top-[4.6%]">
-                      <LaptopSlider />
-                    </div>
-                  </div>
-                </div>
+            <div className=" mx-10 md:mx-12 flex flex-col medium:flex-row items-center justify-between">
+              <div className="">
+                <h1 className="max-w-xl text-4xl leading-[1.1] md:text-5xl md:leading-[1.3] font-bold text-white dark:text-white">
+                  Hey{" "}
+                  <span className="text-black dark:text-yellow-200">Sara!</span>{" "}
+                  seems you haven't enrolled yet
+                </h1>
+                <p className="text-[#FFB453] font-semibold mt-4 dark:text-[#e87f7f] md:text-[28px] max-w-sm mb-5">
+                  Don't hesitate - Join now and get the finest available
+                </p>
+                <button className="dark:bg-orange-600 bg-orange-500 shadow-lg hover:bg-orange-600 dark:hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-lg md:mt-0 mt-10">
+                  Home
+                </button>
               </div>
-              <Footer theme={theme} />
+              <LaptopSlider />
+            </div>
+            <div className="absolute bottom-0 left-0 w-full z-10">
+              <svg
+                width="100%"
+                height="100%"
+                id="svg"
+                viewBox="0 0 1440 320"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <defs>
+                  <linearGradient
+                    id="gradient"
+                    x1="0%"
+                    y1="50%"
+                    x2="100%"
+                    y2="50%"
+                  >
+                    <stop offset="0%" stopColor="#ED374D"></stop>
+                    <stop offset="51%" stopColor="#FA793F"></stop>
+                    <stop offset="100%" stopColor="#FCB900"></stop>
+                  </linearGradient>
+                </defs>
+                <path
+                  d="M0,160L40,181.3C80,203,160,245,240,261.3C320,277,400,267,480,245.3C560,224,640,192,720,197.3C800,203,880,245,960,256C1040,267,1120,245,1200,218.7C1280,192,1360,160,1400,144L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+                  stroke="none"
+                  strokeWidth="0"
+                  fill={theme === "dark" ? "#232222" : "#E6E6E6"}
+                  fillOpacity="1"
+                ></path>
+              </svg>
             </div>
           </div>
-        </>
+          <Footer theme={theme} />
+        </div>
       )}
     </div>
   );
